91fdd2de84a79f3d38ef2a76e6dd444051a2835c
BBaoVanC <bbaovanc@bbaovanc.com>
2022-09-02 23:11:46 -0500

    Use UploadError

--- src/api/v1.rs
+++ src/api/v1.rs
@@ -6,24 +6,31 @@ use hyper::{Body, Request, StatusCode};
 use thiserror::Error;
 
 use crate::backend::UploadFile;
+// use axum::response::Result;
 
 #[derive(Debug, Error)]
 pub enum UploadError {
     #[error("error parsing multipart form data")]
     FormParseError(#[from] MultipartError),
 }
+impl IntoResponse for UploadError {
+    fn into_response(self) -> Response {
+        match self {
+            UploadError::FormParseError(e) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("error parsing form data: {}", e),
+            ),
+        }
+        .into_response()
+    }
+}
 
 /// Accepts: `multipart/form-data`
 ///
 /// Each form field should be a file to upload. The `name` header is ignored.
-async fn upload_post(mut form: Multipart) -> axum::response::Result<impl IntoResponse> {
+async fn upload_post(mut form: Multipart) -> Result<impl IntoResponse, UploadError> {
     let mut files: Vec<UploadFile> = Vec::new();
-    while let Some(field) = form.next_field().await.map_err(|_| {
-        (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            "error parsing multipart form data",
-        )
-    })? {
+    while let Some(field) = form.next_field().await? {
         if field.content_type().is_none() {
             continue;
         }
