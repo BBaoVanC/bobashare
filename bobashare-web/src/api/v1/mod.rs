55f0796c8bc887841525ab7944fb4c86374bfd6c
BBaoVanC <bbaovanc@bbaovanc.com>
2022-09-27 10:59:40 -0500

    a bobashare-web

--- bobashare-web/src/api/v1/mod.rs
+++ bobashare-web/src/api/v1/mod.rs
@@ -1,5 +1,6 @@
 use std::io::{self, ErrorKind};
 
+use axum::response::{IntoResponse, Response};
 use hyper::StatusCode;
 use serde::{Deserialize, Serialize};
 
@@ -7,22 +8,27 @@ pub mod upload;
 
 pub type Result<T = ApiSuccessV1, E = ApiErrorV1> = std::result::Result<T, E>;
 
-// TODO: better name for struct
-pub struct ApiResponse(StatusCode, ApiResponseV1);
-impl From<io::Error> for ApiResponse {
+#[derive(Debug)]
+pub struct ApiResponseV1(StatusCode, ApiResultV1);
+impl IntoResponse for ApiResponseV1 {
+    fn into_response(self) -> Response {
+        (self.0, self.1.into_response())
+    }
+}
+impl From<io::Error> for ApiResponseV1 {
     fn from(err: io::Error) -> Self {
         let code = match err.kind() {
             // TODO: match many of the `ErrorKind`s to different error responses
             _ => StatusCode::INTERNAL_SERVER_ERROR,
         };
 
-        // Self::Error { code, err: ApiErrorV1 { message: err.to_string() } }
+        Self(code, ApiResultV1::Error(ApiErrorV1 { message: err.to_string() }))
     }
 }
 
 #[derive(Debug, Serialize)]
 #[serde(tag = "type")]
-pub enum ApiResponseV1 {
+pub enum ApiResultV1 {
     #[serde(rename = "success")]
     Success(ApiSuccessV1),
     #[serde(rename = "error")]
@@ -30,7 +36,8 @@ pub enum ApiResponseV1 {
 }
 
 #[derive(Debug, Serialize)]
-pub enum ApiSuccessV1 {}
+pub enum ApiSuccessV1 {
+}
 
 #[derive(Debug, Serialize)]
 pub struct ApiErrorV1 {
