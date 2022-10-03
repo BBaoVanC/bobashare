e6d1f0ef0a80281333053b238313f84caa4db47b
BBaoVanC <bbaovanc@bbaovanc.com>
2022-10-02 20:15:07 -0500

    WIP web

--- bobashare-web/src/api/v1/upload.rs
+++ bobashare-web/src/api/v1/upload.rs
@@ -1,16 +1,17 @@
-use std::{cmp::Ordering, collections::HashMap, sync::Arc};
+use std::{cmp::Ordering, sync::Arc};
 
 use axum::{
-    body::Bytes,
-    extract::{multipart::MultipartError, Multipart},
+    extract::{multipart::MultipartError, BodyStream, Multipart},
     response::{IntoResponse, Response, Result},
     Extension, Json,
 };
-use bobashare::storage::{file::CreateUploadError, upload::Upload};
+use bobashare::storage::file::CreateUploadError;
 use chrono::{DateTime, Duration, Utc};
+use futures_util::StreamExt;
 use hyper::{header, Body, HeaderMap, Request, StatusCode};
 use serde::Serialize;
 use serde_json::json;
+use thiserror::Error;
 
 use crate::AppState;
 
@@ -34,52 +35,58 @@ pub struct FileResponse {
     mimetype: String,
 }
 
-#[derive(Debug)]
-pub struct UploadError {
-    code: StatusCode,
-    message: String,
+#[derive(Debug, Error)]
+pub enum UploadError {
+    #[error("an upload at the url already exists")]
+    AlreadyExists,
+    #[error("error creating upload: {0}")]
+    CreateUpload(CreateUploadError),
+    #[error("failed to parse {name} header: {source}")]
+    ParseHeader { name: String, source: anyhow::Error },
+    #[error("error parsing multipart form data: {0}")]
+    Multipart(#[from] MultipartError),
+    #[error("missing Content-Type in the multipart field {0}")]
+    MissingMultipartContentType(String),
+    #[error("missing Content-Disposition (filename) in the multipart field {0}")]
+    MissingMultipartFilename(String),
+    #[error("requested expiry is too long: {0}")]
+    ExpiryTooLong(Duration),
 }
 impl IntoResponse for UploadError {
     fn into_response(self) -> Response {
-        let message = Json(json!({"status": "error", "message": self.message}));
-        (self.code, message).into_response()
-    }
-}
-impl From<MultipartError> for UploadError {
-    fn from(err: MultipartError) -> Self {
-        Self {
-            code: StatusCode::BAD_REQUEST,
-            message: err.to_string(),
-        }
+        // let message = Json(json!({"status": "error", "message": self.message}));
+        // (self.code, message).into_response()
+        let code = match &self {
+            UploadError::AlreadyExists => StatusCode::FORBIDDEN,
+            UploadError::CreateUpload(_) => StatusCode::INTERNAL_SERVER_ERROR, /* TODO: match */
+            // io_error_more
+            // errors
+            UploadError::Multipart(_) => StatusCode::BAD_REQUEST,
+            UploadError::ParseHeader { name: _, source: _ } => StatusCode::BAD_REQUEST,
+            UploadError::MissingMultipartContentType(_)
+            | UploadError::MissingMultipartFilename(_) => StatusCode::BAD_REQUEST,
+            UploadError::ExpiryTooLong(_) => StatusCode::BAD_REQUEST,
+        };
+        let message = Json(json!({
+            "status": "error",
+            "message": self.to_string()
+        }));
+
+        (code, message).into_response()
     }
 }
 impl From<CreateUploadError> for UploadError {
     fn from(err: CreateUploadError) -> Self {
         match err {
-            CreateUploadError::AlreadyExists => Self {
-                code: StatusCode::FORBIDDEN,
-                message: String::from("an upload with the requested name already exists"),
-            },
-            CreateUploadError::Io(e) => {
-                // See https://github.com/rust-lang/rust-clippy/issues/9575
-                #[allow(clippy::match_single_binding)]
-                let code = match e.kind() {
-                    // TODO: track io_error_more and once it's added, match the relevant errors
-                    _ => StatusCode::INTERNAL_SERVER_ERROR,
-                };
-
-                Self {
-                    code,
-                    message: e.to_string(),
-                }
-            }
+            CreateUploadError::AlreadyExists => Self::AlreadyExists,
+            _ => Self::CreateUpload(err),
         }
     }
 }
 
 /// Create an upload that contains a single file
 ///
-/// PUT /api/v1/upload
+/// POST /api/v1/upload
 ///
 /// # Headers
 ///
@@ -93,12 +100,13 @@ impl From<CreateUploadError> for UploadError {
 /// # Success
 ///
 /// - 201 Created
+/// - `Content-Location` header contains the upload URL as well
 /// - Body is JSON, see [`UploadResponse`]
-pub async fn put(
+pub async fn post_single_file(
     state: Extension<Arc<AppState>>,
     // headers: HeaderMap,
     // body: Bytes,
-    request: Request<Body>, // ) -> Result<Json<UploadResponse>, UploadError> {
+    mut request: Request<BodyStream>, // ) -> Result<Json<UploadResponse>, UploadError> {
 ) -> Result<impl IntoResponse, UploadError> {
     // let expiry = request
     //     .headers()
@@ -110,14 +118,14 @@ pub async fn put(
         Some(e) => {
             let expiry = e
                 .to_str()
-                .map_err(|e| UploadError {
-                    code: StatusCode::BAD_REQUEST,
-                    message: format!("Error parsing Bobashare-Expiry header: {}", e),
+                .map_err(|e| UploadError::ParseHeader {
+                    name: String::from("Bobashare-Expiry"),
+                    source: e.into(),
                 })?
                 .parse::<i64>()
-                .map_err(|e| UploadError {
-                    code: StatusCode::BAD_REQUEST,
-                    message: format!("Error parsing Bobashare-Expiry header: {}", e),
+                .map_err(|e| UploadError::ParseHeader {
+                    name: String::from("Bobashare-Expiry"),
+                    source: e.into(),
                 })?;
 
             match expiry.cmp(&0) {
@@ -133,7 +141,9 @@ pub async fn put(
         .create_upload_random_name(state.url_length, expiry)
         .await?;
 
+    todo!();
     Ok((
+        StatusCode::CREATED,
         [(header::CONTENT_LOCATION, upload.metadata.url.clone())],
         Json(UploadResponse {
             url: upload.metadata.url,
@@ -142,53 +152,60 @@ pub async fn put(
     ))
 }
 
-/// Accepts: `multipart/form-data`
+/// Create an upload
 ///
-/// Each form field should be a file to upload. The `name` header is ignored.
+/// # Headers
+///
+/// - `Bobashare-Expiry` -- number -- amount of seconds until the upload should
+///   expire
+///   - specify `0` for no expiry
+///
+/// # Body `multipart/form-data`
+///
+/// Should contain one field per file to upload. No other fields should be
+/// provided.
 pub async fn post(
     state: Extension<Arc<AppState>>,
+    headers: HeaderMap,
     mut form: Multipart,
 ) -> Result<Json<UploadResponse>, UploadError> {
-    // need function to set duration after the fact
-    let mut name: Option<String> = None;
-    let mut duration: Option<Duration> = None;
-    // let mut files = Vec::new();
+    let expiry = match headers.get("Bobashare-Expiry") {
+        None => Some(state.default_expiry),
+        Some(e) => {
+            let expiry = e
+                .to_str()
+                .map_err(|e| UploadError::ParseHeader {
+                    name: String::from("Bobashare-Expiry"),
+                    source: e.into(),
+                })?
+                .parse::<u32>()
+                .map_err(|e| UploadError::ParseHeader {
+                    name: String::from("Bobashare-Expiry"),
+                    source: e.into(),
+                })?;
 
-    // while let Some(field) = form.next_field().await? {
-    //     match field.name().ok_or(UploadInternalError {
-    //         code: StatusCode::BAD_REQUEST,
-    //         message: String::from("received a multipart field with no name"),
-    //     })? {
-
-    //     }
-    // }
+            if expiry == 0 {
+                None
+            } else {
+                let duration = Duration::seconds(expiry.into());
+                if duration > state.max_expiry {
+                    return Err(UploadError::ExpiryTooLong(duration));
+                } else {
+                    Some(duration)
+                }
+            }
+        }
+    };
 
     let mut upload = state
         .backend
-        .create_upload("abc123xyz", Some(Duration::hours(1)))
+        .create_upload_random_name(state.url_length, expiry)
         .await?;
-    // .await
-    // .map_err(|e| match e {
-    //     CreateUploadError::AlreadyExists => {
-    //         (StatusCode::FORBIDDEN, String::from("already exists"))
-    //     }
-    //     CreateUploadError::IoError(e) => (StatusCode::INTERNAL_SERVER_ERROR,
-    // e.to_string()), });
+
     let mut i = 0;
-    // while let Some(mut field) = form.next_field().await? {
     while let Some(field) = form.next_field().await? {
         i += 1; // starts at 1
-        if field.content_type().is_none() {
-            continue;
-        }
-        if field.file_name().is_none() {
-            continue;
-        }
-        let _mimetype = field.content_type().unwrap();
-        let _filename = field.file_name().unwrap();
 
-        // let name_on_disk = format!("{:0<4}", 5);
-        println!("{}", i); // TODO: remove this
         todo!();
     }
 
