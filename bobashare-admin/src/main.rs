fbbd883ccc00edef7921267e845d5a7b82b29b41
BBaoVanC <bbaovanc@bbaovanc.com>
2022-10-06 15:24:04 -0500

    WIP admin

--- bobashare-admin/src/main.rs
+++ bobashare-admin/src/main.rs
@@ -7,6 +7,7 @@ use bobashare::storage::file::FileBackend;
 // use chrono::{prelude::*, Duration};
 use clap::{Parser, Subcommand};
 use cli::create::CreateUpload;
+use tracing_subscriber::{prelude::__tracing_subscriber_SubscriberExt, util::SubscriberInitExt};
 
 pub(crate) mod cli;
 
@@ -25,6 +26,12 @@ pub(crate) enum Command {
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     // TODO: set up logging
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::new(
+            std::env::var("RUST_LOG").unwrap_or_else(|_| "debug".into()),
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
     let cli = Cli::parse();
     let backend = FileBackend::new(cli.root)
