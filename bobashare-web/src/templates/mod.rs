752e0d60f02290a2b40d3313c1002c899b256b53
BBaoVanC <bbaovanc@bbaovanc.com>
2022-10-21 09:09:38 -0500

    Sort of works

--- bobashare-web/src/templates/mod.rs
+++ bobashare-web/src/templates/mod.rs
@@ -1,8 +1,18 @@
 use askama::Template;
+use axum::response::IntoResponse;
+use hyper::header;
 
 #[derive(Template)]
 #[template(path = "error.html")]
 pub struct ErrorTemplate<'t> {
     pub title: &'t str,
     pub message: &'t str,
+}
+impl IntoResponse for ErrorTemplate<'_> {
+    fn into_response(self) -> axum::response::Response {
+        (
+            [(header::CONTENT_TYPE, Self::MIME_TYPE)],
+            self.render().unwrap(),
+        ).into_response()
+    }
 }
