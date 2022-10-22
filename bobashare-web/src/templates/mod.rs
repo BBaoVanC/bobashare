bd9080cbfb0c43a01d2aa0ac0bd2092d4fdc14db
BBaoVanC <bbaovanc@bbaovanc.com>
2022-10-22 11:53:28 -0500

    Fix updated askama_axum dependency

--- bobashare-web/src/templates/mod.rs
+++ bobashare-web/src/templates/mod.rs
@@ -1,25 +1,8 @@
 use askama::Template;
-use axum::{
-    http::HeaderValue,
-    response::{IntoResponse, Response},
-};
-use hyper::header;
 
 #[derive(Template)]
 #[template(path = "error.html")]
 pub struct ErrorTemplate<'t> {
     pub title: &'t str,
     pub message: &'t str,
-}
-impl IntoResponse for ErrorTemplate<'_> {
-    fn into_response(self) -> Response {
-        (
-            [(
-                header::CONTENT_TYPE,
-                HeaderValue::from_static(Self::MIME_TYPE),
-            )],
-            self.render().unwrap(),
-        )
-            .into_response()
-    }
 }
