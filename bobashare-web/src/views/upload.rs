bd765e4bc9b65ef3542256e7de400e9061514111
BBaoVanC <bbaovanc@bbaovanc.com>
2022-11-03 17:26:21 -0500

    Fix uncapitalized TODO [skip ci]

--- bobashare-web/src/views/upload.rs
+++ bobashare-web/src/views/upload.rs
@@ -83,7 +83,7 @@ pub async fn upload(state: State<Arc<AppState>>) -> Result<impl IntoResponse, Er
                 true
             }
         }).collect(),
-        // tODO: make never expiry work
+        // TODO: make never expiry work
         never_expiry_allowed: state.max_expiry.is_none(),
         state: state.0.into(),
     })
