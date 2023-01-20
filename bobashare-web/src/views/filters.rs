4d6f3dc43978082093771d724a92ec41f52428ca
BBaoVanC <bbaovanc@bbaovanc.com>
2023-01-20 08:22:16 -0600

    Remove unnecessary .into() in humanduration

--- bobashare-web/src/views/filters.rs
+++ bobashare-web/src/views/filters.rs
@@ -5,20 +5,20 @@ use humansize::format_size;
 pub fn humanduration(duration: &Duration) -> Result<String> {
     let duration = *duration;
     if duration < Duration::minutes(1) {
-        return Ok(format!("{} seconds", duration.num_seconds()).into());
+        return Ok(format!("{} seconds", duration.num_seconds()));
     }
     if duration < Duration::hours(1) {
-        return Ok(format!("{} minutes", duration.num_minutes()).into());
+        return Ok(format!("{} minutes", duration.num_minutes()));
     }
     if duration < Duration::days(1) {
-        return Ok(format!("{} hours", duration.num_hours()).into());
+        return Ok(format!("{} hours", duration.num_hours()));
     }
     // using weeks is dumb, let's use days up until a month
     if duration < Duration::days(30) {
-        return Ok(format!("{} days", duration.num_days()).into());
+        return Ok(format!("{} days", duration.num_days()));
     }
     // I think most people would assume a month is about 30 days
-    return Ok(format!("{} months", duration.num_days() / 30).into());
+    return Ok(format!("{} months", duration.num_days() / 30));
     // probably no point going past months
 }
 
