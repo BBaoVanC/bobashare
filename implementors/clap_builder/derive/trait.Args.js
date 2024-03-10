ec5785254cd9872c3c2fabfdcdb8afc2bbc2112e
BBaoVanC <BBaoVanC@users.noreply.github.com>
2024-03-10 06:09:11 +0000

    deploy: 1d210a26cea457f5a0df33ff9979df11c656e397

--- implementors/clap_builder/derive/trait.Args.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"bobashare_admin":[["impl Args for <a class=\"struct\" href=\"bobashare_admin/cli/create/struct.CreateUpload.html\" title=\"struct bobashare_admin::cli::create::CreateUpload\">CreateUpload</a>"],["impl Args for <a class=\"struct\" href=\"bobashare_admin/struct.Cli.html\" title=\"struct bobashare_admin::Cli\">Cli</a>"],["impl Args for <a class=\"struct\" href=\"bobashare_admin/cli/cleanup/struct.Cleanup.html\" title=\"struct bobashare_admin::cli::cleanup::Cleanup\">Cleanup</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ trait.impl/clap_builder/derive/trait.Args.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"bobashare_admin":[["impl Args for <a class=\"struct\" href=\"bobashare_admin/cli/cleanup/struct.Cleanup.html\" title=\"struct bobashare_admin::cli::cleanup::Cleanup\">Cleanup</a>"],["impl Args for <a class=\"struct\" href=\"bobashare_admin/struct.Cli.html\" title=\"struct bobashare_admin::Cli\">Cli</a>"],["impl Args for <a class=\"struct\" href=\"bobashare_admin/cli/create/struct.CreateUpload.html\" title=\"struct bobashare_admin::cli::create::CreateUpload\">CreateUpload</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
